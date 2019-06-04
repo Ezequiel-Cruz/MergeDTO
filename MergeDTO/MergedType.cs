53d366565c5df0c3c5095b643e05bdbe439c4fb4
Ezequiel Teixeira Cruz <ezequiel.capgemini@CHI00557796>
2019-06-04 10:46:42 -0300

    Add project files.

---
+++ MergeDTO/MergedType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace MergeDTO
+{
+    class MergedType<T1, T2> : DynamicObject
+    {
+        T1 t1;
+        T2 t2;
+        Dictionary<string, object> members = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
+
+        public MergedType(T1 t1, T2 t2)
+        {
+            this.t1 = t1;
+            this.t2 = t2;
+            foreach (System.Reflection.PropertyInfo fi in typeof(T1).GetProperties())
+            {
+                members[fi.Name] = fi.GetValue(t1, null);
+            }
+            foreach (System.Reflection.PropertyInfo fi in typeof(T2).GetProperties())
+            {
+                members[fi.Name] = fi.GetValue(t2, null);
+            }
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            string name = binder.Name.ToLower();
+            return members.TryGetValue(name, out result);
+        }
+    }
+}
---
+++ MergeDTO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MergeDTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source1 = new
+            {
+                foo = "foo",
+                bar = "bar",
+                teste = "0"
+            };
+
+            var source2 = new
+            {
+                baz = "baz",
+                bar = "Onde passa",
+                teste = "5578",
+                ruiva = "alto"
+            };
+
+            dynamic merged = Merge(source1, source2);
+
+            Console.WriteLine("{0} {1} {2} {3} {4}", merged.foo, merged.bar, merged.baz, merged.teste, merged.ruiva);
+            Console.Read();
+        }
+
+        static MergedType<T1, T2> Merge<T1, T2>(T1 t1, T2 t2)
+        {
+            return new MergedType<T1, T2>(t1, t2);
+        }
+    }
+}
